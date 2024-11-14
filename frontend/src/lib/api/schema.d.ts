0e18157d17e24d086e88d8887899fd9bf7725ad9
Brendan Mullany <bmullany@drew.edu>
2024-11-14 16:07:08 -0500

    reran gen, fixed schema

--- frontend/src/lib/api/schema.d.ts
+++ frontend/src/lib/api/schema.d.ts
@@ -139,6 +139,76 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/verify/list": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description List all unverfied staff */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["UnverifiedStaffInfo"][];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/tenants/list": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description List all of the Tenants (user table for now) */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["TenantsInfo"][];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -159,6 +229,21 @@ export interface components {
         };
         RegisterResult: {
             message: string;
+        };
+        UnverifiedStaffInfo: {
+            staffID: number;
+            /** Format: email */
+            email: string;
+            firstName: string;
+            lastName: string;
+        };
+        TenantsInfo: {
+            id: number;
+            username: string;
+            password: string;
+            active: boolean;
+            firstname: string;
+            lastname: string;
         };
     };
     responses: never;
